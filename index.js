c7dcfa819246f08bf1de181c9e3e6f01ef644f27
KareimGazer <kareimgazar1990@gmail.com>
2024-07-06 10:07:32 +0300

    added cors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const generateRandomId = require('./utils')
 var path = require('path')
 var fs = require('fs')
 const morgan = require('morgan')
+const cors = require('cors')
 const PORT = 3001
 
 // create a write stream (in append mode)
@@ -12,6 +13,8 @@ var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
 
 morgan.token('req-body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body', { stream: accessLogStream }))
+
+app.use(cors())
 
 app.get('/api/persons', (req, res) => {
     res.json(contacts)
